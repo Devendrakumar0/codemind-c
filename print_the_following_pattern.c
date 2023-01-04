233eadcc6957bacbf028421574a4cd226d913b58
Devendrakumar0 <84584733+Devendrakumar0@users.noreply.github.com>
2023-01-04 10:35:38 +0530

    #include<iostream> using namespace std; int main(){     int n;     cin>>n;     for(int i=1;i<=n;i++){         for(int j=n;j>=1;j--){             cout<<j<<" ";         }         cout<<endl;     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,9 +3,9 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=0;i<n;i++){
-        for(int j=1;j<=n-i;j++){
-            cout<<j;
+    for(int i=1;i<=n;i++){
+        for(int j=n;j>=1;j--){
+            cout<<j<<" ";
         }
         cout<<endl;
     }
